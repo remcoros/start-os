193d7d0ae0f3b537c2fcae57cf337f1cbfe19a1d
Remco Ros <remcoros@live.nl>
2025-01-06 19:54:16 +0100

    add r8168-dkms and pull from debian trixie repo

--- image-recipe/build.sh
+++ image-recipe/build.sh
@@ -44,7 +44,7 @@ if [ "$NON_FREE" = 1 ]; then
 	if [ "$IB_SUITE" = "bullseye" ]; then
 		ARCHIVE_AREAS="$ARCHIVE_AREAS non-free"
 	elif [ "$IB_SUITE" = "bookworm" ]; then
-		ARCHIVE_AREAS="$ARCHIVE_AREAS non-free-firmware"
+		ARCHIVE_AREAS="$ARCHIVE_AREAS non-free non-free-firmware"
 	fi
 fi
 
@@ -158,14 +158,23 @@ echo "deb [arch=${IB_TARGET_ARCH} signed-by=/etc/apt/trusted.gpg.d/tor.key.gpg] 
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o config/archives/docker.key
 echo "deb [arch=${IB_TARGET_ARCH} signed-by=/etc/apt/trusted.gpg.d/docker.key.gpg] https://download.docker.com/linux/debian ${IB_SUITE} stable" > config/archives/docker.list
 
-echo "deb [check-valid-until=no] https://snapshot.debian.org/archive/debian/20230922T024957Z trixie main contrib" > config/archives/trixie.list
+echo "deb https://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware" > config/archives/trixie.list
+echo "deb [check-valid-until=no] https://snapshot.debian.org/archive/debian/20230922T024957Z trixie main contrib" > config/archives/podman.list
 cat > config/archives/trixie.pref <<- EOF
 Package: *
 Pin: release n=trixie
 Pin-Priority: 100
 
 Package: podman
-Pin: release n=trixie
+Pin: version 4.5.1*
+Pin-Priority: 600
+
+Package: firmware-realtek
+Pin: version 20241210
+Pin-Priority: 600
+
+Package: r8168-dkms
+Pin: version 8.054.*
 Pin-Priority: 600
 EOF
 
@@ -176,7 +185,7 @@ dpkg-deb --fsys-tarfile $base_dir/deb/${IMAGE_BASENAME}.deb | tar --to-stdout -x
 
 ## Firmware
 if [ "$NON_FREE" = 1 ]; then
-	echo 'firmware-iwlwifi firmware-misc-nonfree firmware-brcm80211 firmware-realtek firmware-atheros firmware-libertas firmware-amd-graphics' > config/package-lists/nonfree.list.chroot
+	echo 'firmware-iwlwifi firmware-misc-nonfree firmware-brcm80211 firmware-realtek firmware-atheros firmware-libertas firmware-amd-graphics r8168-dkms' > config/package-lists/nonfree.list.chroot
 fi
 
 if [ "${IB_TARGET_PLATFORM}" = "raspberrypi" ]; then
